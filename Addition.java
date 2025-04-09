e1c260bcc41001094fae5109fd937e742652a704
01fe23bca056 <01fe23bca056@kletech.ac.in>
2025-04-09 15:18:37 +0530

    save

---
+++ Addition.java
@@ -0,0 +1,9 @@
+class Addition {
+    public static void main(String[] args) {
+        int a, b;
+        a = 50;
+        b = 30;
+        int sum = a + b;
+        System.out.println("Addition of a & b is: " + sum);
+    }
+}
