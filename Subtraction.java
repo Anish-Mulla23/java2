cbc8ee8520bb99d39e1e622697a3706a8ff7217d
01fe23bca056 <01fe23bca056@kletech.ac.in>
2025-04-09 15:22:31 +0530

    save

---
+++ Subtraction.java
@@ -0,0 +1,11 @@
+package java1;
+
+public class Subtraction {
+    public static void main(String[] args) {
+        int a, b;
+        a = 50;
+        b = 30;
+        int sub = a -b;
+        System.out.println("Subtraction of a & b is: " + sub);
+    }
+}
